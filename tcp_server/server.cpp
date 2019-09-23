d3d69de037105705ab9176bdad7f37bcd09359b6
Vlado <vladimir.kopceky@epita.fr>
2019-09-23 15:13:09 +0200

    Modified CMakelists

--- tcp_server/server.cpp
+++ tcp_server/server.cpp
@@ -1,3 +1,5 @@
+#include <ostream>
+#include <iostream>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -5,6 +7,31 @@
 #include<linux/tcp.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
+
+using namespace std;
+void print_segment(tcphdr* tcph, unsigned char* buff);
+
+namespace Color {
+    enum Code {
+        FG_RED      = 31,
+        FG_GREEN    = 32,
+        FG_BLUE     = 34,
+        FG_DEFAULT  = 39,
+        BG_RED      = 41,
+        BG_GREEN    = 42,
+        BG_BLUE     = 44,
+        BG_DEFAULT  = 49
+    };
+    class Modifier {
+        Code code;
+    public:
+        Modifier(Code pCode) : code(pCode) {}
+        friend std::ostream&
+        operator<<(std::ostream& os, const Modifier& mod) {
+            return os << "\033[" << mod.code << "m";
+        }
+    };
+}
 
 int main(int argc, char const *argv[])
 {
@@ -67,7 +94,129 @@ int main(int argc, char const *argv[])
       printf("ece: %d\n", tcp->ece);
       printf("Window: %d\n", tcp->window);
 
+      print_segment(tcp, buffer);
+
     }
 
     return 0;
+}
+
+
+void print_segment(tcphdr* tcph, unsigned char* buff){
+
+    Color::Modifier red(Color::FG_RED);
+    Color::Modifier green(Color::FG_GREEN);
+    Color::Modifier def(Color::FG_DEFAULT);
+
+    /*
+    cout << "Source Port: "  << tcph->source_port << endl;
+    cout << "Dest Port: "  << tcph->dest_port << endl;
+    cout << "CWR: "  << to_string(tcph->cwr) << endl;
+    cout << "ECN: "  << to_string(tcph->ecn) << endl;
+    cout << "URG: "  << to_string(tcph->urg) << endl;
+    cout << "ACK: "  << to_string(tcph->ack) << endl;
+    cout << "PSH: "  << to_string(tcph->psh) << endl;
+    cout << "RST: "  << to_string(tcph->rst) << endl;
+    cout << "SYN: "  << to_string(tcph->syn) << endl;
+    cout << "FIN: "  << to_string(tcph->fin) << endl;
+    cout << "NS: "  << to_string(tcph->ns) << endl;
+    cout << "Sequence: "  << tcph->sequence << endl;
+    cout << "ACKNOWLEDGE: "  << tcph->acknowledge << endl;
+    cout << "Reserved Part1: "  << to_string(tcph->reserved_part1) << endl;
+    cout << "Data offset: "  << to_string(tcph->data_offset) << endl;
+    cout << "Window: "  << tcph->window << endl;
+    cout << "Checksum: "  << tcph->checksum << endl;
+    cout << "Urgent Pointer: "  << tcph->urgent_pointer << endl;
+*/
+
+    cout << green << " 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 " << endl;
+    cout << green << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
+    cout << green << "|        Source Port            |        Destination Port       |" << endl;
+    cout << green << "|        ";
+
+    cout << red << ntohs(tcph->source);
+
+    cout << green << "                   |        ";
+
+    cout << red << ntohs(tcph->dest);
+
+    cout << green << "                  |" << endl;
+    cout << green << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
+    cout << green << "|                       Sequence  Number                        |" << endl;
+    cout << green << "|                       ";
+
+    cout << red << tcph->seq;
+
+    cout << green <<"                                     |" << endl;
+    cout << green << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
+    cout << green << "|                     Acknowledgment Number                     |" << endl;
+    cout << green <<"|                     ";
+
+    cout << red << tcph->ack_seq;
+
+    cout << green <<"                                         |" << endl;
+    cout << green << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
+    cout << green <<"| Offset|  Reserved |   Flags   |             Window            |" << endl;
+    cout << green <<"|       |           |U|A|P|R|S|F|                               |" << endl;
+    cout << green <<"|       |           |R|C|S|S|Y|I|                               |" << endl;
+    cout << green <<"|       |           |G|K|H|T|N|N|                               |" << endl;
+    cout << green <<"|   ";
+
+    cout << red << to_string(tcph->doff);
+
+    cout << green << "   |     ";
+
+    cout << red << to_string(tcph->res1);
+
+    cout << green << "     |";
+
+    cout<< red <<to_string(tcph->urg);
+
+    cout << green <<"|";
+
+    cout << red << to_string(tcph->ack);
+
+    cout << green << "|";
+
+    cout << red <<  to_string(tcph->psh);
+
+    cout << green << "|";
+
+    cout << red << to_string(tcph->rst);
+
+    cout << green <<"|";
+
+    cout << red << to_string(tcph->syn);
+
+    cout << green << "|";
+
+    cout << red << to_string(tcph->fin);
+
+    cout << green << "|";
+
+    cout << "             " << red << tcph->window;
+
+    cout << green << "                 |" << endl;
+    cout << green << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
+    cout << green << "|            Checksum           |         Urgent Pointer        |" << endl;
+    cout << green <<"|            ";
+
+    cout << red << tcph->check;
+
+    cout << green << "                  |            ";
+
+    cout << red << tcph->urg_ptr;
+
+    cout << green << "                  |" << endl;
+    cout << green << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
+    cout << green << "|                    Options                    |    Padding    |" << endl;
+    cout << green << "|                                               |               |" << endl;
+    cout << green << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
+
+
+
+    // cout << buff + sizeof(ip_hdr) << endl;
+    cout << green <<"Payload ===>  " ;
+
+    cout << red << buff + sizeof(tcphdr) << endl;
 }
